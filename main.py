aa9ea7500e00763cde606c3d9c6daaebd43ec847
Alex-MSX <alejandro.moreno.seba@gmail.com>
2024-02-16 10:16:56 -0600

    Se agregó la función saludar a main.py

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ def sumar(*args):
     return sum(args)
 
 
+def saludar(nombre):
+    print(f'Hola {nombre}! Cómo estás?')
+
+
 suma = sumar(5,6,8,2,1,4,5,6,3)
 print(suma)
 
+
+saludar('Alejandro')
